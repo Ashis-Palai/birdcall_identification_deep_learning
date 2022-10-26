537c38d2ff836e63ac8122d757493a35962f4720
Ashis Palai <113517524+Ashis-Palai@users.noreply.github.com>
2022-10-26 08:37:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,5 +133,5 @@ def predict():
     
     return 'Hare Krishna Hare Krishna Krishna Krishna Hare Hare \n Hare Rama Hare Rama Rama Rama Hare Hare'
 # 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000 , debug=True)
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port=8000 , debug=True)
