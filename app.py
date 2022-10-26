15a6648c0c37f6a5a5e62dcee242bec1c6e67385
Ashis Palai <113517524+Ashis-Palai@users.noreply.github.com>
2022-10-26 09:20:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,6 +134,4 @@ def predict():
     return 'Hare Krishna Hare Krishna Krishna Krishna Hare Hare \n Hare Rama Hare Rama Rama Rama Hare Hare'
 
 if __name__ == '__main__':
-    port = 5000 + random.randint(0, 999)
-    url = "http://127.0.0.1:{0}".format(port)
-    app.run(use_reloader=False, debug=True, port=port)
+    app.run()
